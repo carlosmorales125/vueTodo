927026fe2aa254497d7be77ccb2ea57a5ee59ac3
Morales <cmorales1@MATL2232CMORAL.local>
2019-01-02 14:26:06 -0500

    first draft of todo form

--- src/components/TodoListForm/Form.vue
+++ src/components/TodoListForm/Form.vue
@@ -1,11 +1,28 @@
 <template>
-    <div>HI! I'm the form!</div>
+    <form>
+        <div class="form-group row">
+            <input type="text" class="form-control" id="newTask" :placeholder="inputVal">
+            <button type="submit" class="btn btn-primary">Add Task</button>
+        </div>
+    </form>
 </template>
 <script>
-    export default {
-      name: 'form',
-    };
+  export default {
+    name: 'form',
+    data() {
+      return {
+        inputVal: 'Enter a New Task'
+      }
+    }
+  };
 </script>
 <style>
+    form .form-group button {
+        width: 100%;
+    }
 
+    form {
+        margin: 0 auto;
+        max-width: 600px;
+    }
 </style>
